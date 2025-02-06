d0a25fb392f816e93f4bb8024b71d681c23ab4e7
George Sobhy <George@oasisoft.net>
2025-02-06 08:58:49 +0200

    Adding project files

---
+++ Getting Started with JSON in C# Using Json.NET 12/Newton Json/Newton Json/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Newton_Json
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
